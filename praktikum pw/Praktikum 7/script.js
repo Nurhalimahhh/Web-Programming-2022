3d2639ad6fca0fa7e197cd52bd05b64469151c97
Nurhalimah <92778748+Nurhalimahhh@users.noreply.github.com>
2021-12-31 23:10:14 +0700

    Add files via upload

---
+++ praktikum pw/Praktikum 7/script.js
@@ -0,0 +1,13 @@
+console.log('Helo, Nur') ;
+
+// belajar variable
+// statis
+const phi = 3.14;
+// phi = 3.15; eror tidak bisa diberi nilai ulang
+console. log(phi);
+
+
+// dynamis
+let age = 18;
+age = 17;
+console. log(age);
